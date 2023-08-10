2a66ff5de95c66c74faa9ecdb7666bb9434e47fb
Henrymelendez <henryomarm@gmail.com>
2023-08-10 19:53:17 -0400

    Added Configurations for jwt Interceptor

--- tenant-service/src/main/java/com/rentalApp/tenantservice/config/WebConfig.java
+++ tenant-service/src/main/java/com/rentalApp/tenantservice/config/WebConfig.java
@@ -1,2 +1,14 @@
-package com.rentalApp.tenantservice.config;public class WebConfig {
+package com.rentalApp.tenantservice.config;
+
+import com.rentalApp.tenantservice.config.interceptors.JwtInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JwtInterceptor())
+                .addPathPatterns("/tenant/**");
+    }
 }
